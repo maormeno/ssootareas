585a7e1dadfc3da89c71222f0fa6bec7d988c2c5
Mateo <maormeno@uc.cl>
2021-04-10 17:11:53 -0400

    empezamos

--- Tarea1/base/src/crtree/main.c
+++ Tarea1/base/src/crtree/main.c
@@ -1,7 +1,14 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "../file_manager/manager.h"
 
 
 int main(int argc, char **argv)
 {
-  printf("Hello T1!\n");
+  InputFile* input_file = read_file(argv[1]);
+  for(int i = 0; i < input_file->len; i++)
+  {
+    printf(input_file->lines[i][0]);
+    printf("\n");
+  }
 }
